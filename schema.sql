01d8f23cf42b5d156ade64cb5f2976a860119930
arnar44 <arnar44@gmail.com>
2018-02-16 14:56:01 +0000

    schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 create table info (
     id serial primary key,
-    date TIMESTAMP not null DEFAULT current_timestamp,
+    date timestamp without time zone DEFAULT current_timestamp,
     name varchar(64) not null,
     email varchar(64) not null,
     ssn varchar(64) not null,
